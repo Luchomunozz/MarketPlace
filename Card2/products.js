6987b2f953801ec332c796d4449643fad8b3436c
Luchomunozz <lmmunoz311@misena.edu.co>
2021-11-05 13:50:22 -0500

    LM_1.2_Penultima parte de la página

--- Card2/products.js
+++ Card2/products.js
@@ -1,3 +1,8 @@
 const products = [
-  {id:1,name:'Product',img:'URL',price:500,}
+  {id:1,name:'God of War',img:'URL',price:50000,}
+  {id:2,name:'Rogue Company',img:'URL',price:50000,}
+  {id:3,name:'Rocket League',img:'URL',price:50000,}
+  {id:4,name:'Call of Duty Warzone',img:'URL',price:50000,}
+  {id:5,name:'Resident Evil 3 Remastered',img:'URL',price:50000,}
+  {id:6,name:'Halo Infinite',img:'URL',price:50000,}
 ];
